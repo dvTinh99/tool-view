60635b33c743bab379d76ca43e1095027a4e53f5
dvTinh99 <tinh5969@gmail.com>
2022-11-02 19:46:14 +0700

    add 8,9

---
+++ run/acc_8.js
@@ -0,0 +1,12 @@
+import diana from '../cookie/diana.json' assert { type: "json" };
+import awin from '../cookie/awin.json' assert { type: "json" };
+import man from '../cookie/man.json' assert { type: "json" };
+import nghia2 from '../cookie/nghia2.json' assert { type: "json" };
+import nghia3 from '../cookie/nghia3.json' assert { type: "json" };
+import acc_8 from '../cookie/acc_8.json' assert { type: "json" };
+import main from '../test.js';
+
+main(acc_8.cookies, 'acc_7');
+
+// console.log(process.argv[2]); // arg1
+// console.log(process.argv[3]); // arg2
---
+++ run/acc_9.js
@@ -0,0 +1,12 @@
+import diana from '../cookie/diana.json' assert { type: "json" };
+import awin from '../cookie/awin.json' assert { type: "json" };
+import man from '../cookie/man.json' assert { type: "json" };
+import nghia2 from '../cookie/nghia2.json' assert { type: "json" };
+import nghia3 from '../cookie/nghia3.json' assert { type: "json" };
+import acc_9 from '../cookie/acc_9.json' assert { type: "json" };
+import main from '../test.js';
+
+main(acc_9.cookies, 'acc_7');
+
+// console.log(process.argv[2]); // arg1
+// console.log(process.argv[3]); // arg2
