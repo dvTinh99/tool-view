0fc6f947e0d6c0680d361f83882d56cbb95f1ca5
dvTinh99 <tin5969@gmail.com>
2022-11-02 11:46:02 +0000

    update

--- run/run.sh
+++ run/run.sh
@@ -1,7 +1,7 @@
 sudo rm -r /root/.pm2/logs 
 
-pm2 start acc_1.js --exp-backoff-restart-delay=100 --name $RANDOM --watch
-pm2 start acc_2.js --exp-backoff-restart-delay=100 --name $RANDOM --watch
-pm2 start acc_3.js --exp-backoff-restart-delay=100 --name $RANDOM --watch
-pm2 start acc_4.js --exp-backoff-restart-delay=100 --name $RANDOM --watch
-pm2 start acc_5.js --exp-backoff-restart-delay=100 --name $RANDOM --watch
+pm2 start acc_1.js --exp-backoff-restart-delay=100 --name $RANDOM
+pm2 start acc_2.js --exp-backoff-restart-delay=100 --name $RANDOM
+pm2 start acc_3.js --exp-backoff-restart-delay=100 --name $RANDOM
+pm2 start acc_4.js --exp-backoff-restart-delay=100 --name $RANDOM
+pm2 start acc_5.js --exp-backoff-restart-delay=100 --name $RANDOM
